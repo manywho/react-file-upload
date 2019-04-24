181f5480efc58556ab613a004276242ab35d2764
TomFullalove <tom_fullalove@dell.com>
2019-04-24 15:10:04 +0100

    Replacing login-form details with file-upload

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
         extensions: ['.tsx', '.ts']
     },
     output: {
-        filename: 'react-login-form.js',
+        filename: 'react-file-upload.js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
